17f76db62aa9d2c580cd318fb5a0747b9125653a
Ryan Riley <ryanriley@live.com>
2019-01-05 15:20:10 -0600

    Convert to Frank routing

--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -1,13 +1,14 @@
-open System
 open System.IO
 open System.Threading.Tasks
 
 open Microsoft.AspNetCore
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.DependencyInjection
 
-open FSharp.Control.Tasks.V2
+open Frank.Builder
+open FSharp.Control.Tasks.V2.ContextInsensitive
 open Giraffe
 open Shared
 
@@ -16,29 +17,44 @@ let publicPath = Path.GetFullPath "../Client/public"
 let port = 8085us
 
 let getInitCounter () : Task<Counter> = task { return { Value = 42 } }
-let webApp =
-    route "/api/init" >=>
-        fun next ctx ->
+let hello app =
+    resource "api/hello" app {
+        get (fun (ctx:HttpContext) ->
+            task {
+                ctx.Response.StatusCode <- 200
+                do! ctx.Response.WriteAsync("Hello, world!")
+            })
+    }
+
+let api app =
+    resource "api/init" app {
+        get (fun next ctx ->
             task {
                 let! counter = getInitCounter()
                 return! Successful.OK counter next ctx
-            }
-
-let configureApp (app : IApplicationBuilder) =
-    app.UseDefaultFiles()
-       .UseStaticFiles()
-       .UseGiraffe webApp
+            })
+    }
 
 let configureServices (services : IServiceCollection) =
-    services.AddGiraffe() |> ignore
-    services.AddSingleton<Giraffe.Serialization.Json.IJsonSerializer>(Thoth.Json.Giraffe.ThothSerializer()) |> ignore
+    services.AddGiraffe()
+            .AddSingleton<Giraffe.Serialization.Json.IJsonSerializer>(Thoth.Json.Giraffe.ThothSerializer())
 
-WebHost
-    .CreateDefaultBuilder()
-    .UseWebRoot(publicPath)
-    .UseContentRoot(publicPath)
-    .Configure(Action<IApplicationBuilder> configureApp)
-    .ConfigureServices(configureServices)
-    .UseUrls("http://0.0.0.0:" + port.ToString() + "/")
-    .Build()
-    .Run()
+let initBuilder =
+    WebHost
+        .CreateDefaultBuilder()
+        .UseWebRoot(publicPath)
+        .UseContentRoot(publicPath)
+        .UseUrls("http://0.0.0.0:" + port.ToString() + "/")
+
+let hostBuilder =
+    webHost initBuilder {
+        service configureServices
+
+        plug DefaultFilesExtensions.UseDefaultFiles
+        plug StaticFileExtensions.UseStaticFiles
+
+        route hello
+        route api
+    }
+
+hostBuilder.Build().Run()
