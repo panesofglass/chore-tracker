0cfa976ceb37966710f9309cf014dc642c287bb3
Ryan Riley <ryanriley@live.com>
2019-01-05 15:44:51 -0600

    Update Frank to use configure for web host
    Still need to figure out the reason for the unexpected 405 response
    from Giraffe handler functions.

--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -4,7 +4,6 @@ open System.Threading.Tasks
 open Microsoft.AspNetCore
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
-open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.DependencyInjection
 
 open Frank.Builder
@@ -17,15 +16,6 @@ let publicPath = Path.GetFullPath "../Client/public"
 let port = 8085us
 
 let getInitCounter () : Task<Counter> = task { return { Value = 42 } }
-let hello app =
-    resource "api/hello" app {
-        get (fun (ctx:HttpContext) ->
-            task {
-                ctx.Response.StatusCode <- 200
-                do! ctx.Response.WriteAsync("Hello, world!")
-            })
-    }
-
 let api app =
     resource "api/init" app {
         get (fun next ctx ->
@@ -39,21 +29,19 @@ let configureServices (services : IServiceCollection) =
     services.AddGiraffe()
             .AddSingleton<Giraffe.Serialization.Json.IJsonSerializer>(Thoth.Json.Giraffe.ThothSerializer())
 
-let initBuilder =
-    WebHost
-        .CreateDefaultBuilder()
-        .UseWebRoot(publicPath)
-        .UseContentRoot(publicPath)
-        .UseUrls("http://0.0.0.0:" + port.ToString() + "/")
+let configureWebHost (builder : IWebHostBuilder) =
+    builder.UseWebRoot(publicPath)
+           .UseContentRoot(publicPath)
+           .UseUrls("http://0.0.0.0:" + port.ToString() + "/")
 
 let hostBuilder =
-    webHost initBuilder {
+    webHost (WebHost.CreateDefaultBuilder()) {
+        configure configureWebHost
         service configureServices
 
         plug DefaultFilesExtensions.UseDefaultFiles
         plug StaticFileExtensions.UseStaticFiles
 
-        route hello
         route api
     }
 
