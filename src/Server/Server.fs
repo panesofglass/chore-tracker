eb4cbdcaf72a83750b548cf3fd1a9a3ea27c644c
Ryan Riley <ryanriley@live.com>
2019-01-05 14:37:30 -0600

    Initial commit - generated SAFE template

---
+++ src/Server/Server.fs
@@ -0,0 +1,44 @@
+open System
+open System.IO
+open System.Threading.Tasks
+
+open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+
+open FSharp.Control.Tasks.V2
+open Giraffe
+open Shared
+
+
+let publicPath = Path.GetFullPath "../Client/public"
+let port = 8085us
+
+let getInitCounter () : Task<Counter> = task { return { Value = 42 } }
+let webApp =
+    route "/api/init" >=>
+        fun next ctx ->
+            task {
+                let! counter = getInitCounter()
+                return! Successful.OK counter next ctx
+            }
+
+let configureApp (app : IApplicationBuilder) =
+    app.UseDefaultFiles()
+       .UseStaticFiles()
+       .UseGiraffe webApp
+
+let configureServices (services : IServiceCollection) =
+    services.AddGiraffe() |> ignore
+    services.AddSingleton<Giraffe.Serialization.Json.IJsonSerializer>(Thoth.Json.Giraffe.ThothSerializer()) |> ignore
+
+WebHost
+    .CreateDefaultBuilder()
+    .UseWebRoot(publicPath)
+    .UseContentRoot(publicPath)
+    .Configure(Action<IApplicationBuilder> configureApp)
+    .ConfigureServices(configureServices)
+    .UseUrls("http://0.0.0.0:" + port.ToString() + "/")
+    .Build()
+    .Run()
